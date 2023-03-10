5d99ccf9d10ae20412e023458858d192205cc94a
Jeongil-park <opurpe@gmail.com>
2023-03-10 16:56:10 +0900

    test:새로운 코드인데 모듈 재설치 해야 오류 해결이라 올림

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,13 @@
-import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
-import Login from './components/Login';
-
-function App() {
-  // 토큰이 없으면 로그인 페이지로 이동합니다.
-  const token = localStorage.getItem('token');
-  const isLoggedIn = !!token;
-
+import React from 'react';
+import Login from '@components/Login';
+ 
+function App () {
+ 
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          {isLoggedIn ? <h1>메인 페이지</h1> : <Redirect to="/login" />}
-        </Route>
-        <Route path="/login">
-          {isLoggedIn ? <Redirect to="/" /> : <Login />}
-        </Route>
-      </Switch>
-    </Router>
-  );
+    <div>
+      <Login />
+    </div>
+  )
 }
-
+ 
 export default App;
