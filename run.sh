b7bb8fb2da8b786c84bb15416a4de6db053349a0
Jun0zo <joon0zo1022@gmail.com>
2023-03-12 23:45:52 +0900

    config: fast-api  hot-reload가능하게 변경

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker-compose up
-#docker-compose up --build
+# docker-compose up
+docker-compose up --build
--- test_backend/app/main.py
+++ test_backend/app/main.py
@@ -4,7 +4,7 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World1"}
+    return {"Hello": "test"}
 
 
 @app.get("/items/{item_id}")
