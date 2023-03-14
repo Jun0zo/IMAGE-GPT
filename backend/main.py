f15f743da8c428166977e501d6781ff02c3df6fc
c-wonjun <wonjun000823@gmail.com>
2023-03-14 10:44:30 +0900

    test:git 기능점검을 통한 frontend branch 가져오기

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,31 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.get("/")
+db = [
+    {"id": "choiwonjun", "pw": "1234"},
+    {"id": "jun0jo", "pw": "12345"},
+    {"id": "jeong1park", "pw": "321"},
+]
+
+
+class Login(BaseModel):
+    id: str
+    pw: str
+
+
+@app.post("/")
 async def read_root():
     return {"Hello": "World"}
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
+
+@app.post("/login")
+async def read_login(login: Login):
+    print({"login_id": login.id, "login_pw": login.pw})
+    for user in db:
+        if login.id == user["id"] and login.pw == user["pw"]:
+            print(login.id, login.pw)
+            return {"로그인 성공"}
+    print(login.id, login.pw)
+    return {"로그인 실패"}
