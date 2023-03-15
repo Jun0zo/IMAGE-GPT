69e32e1fcab50fe4392ba3ef2cefcdbb723420df
c-wonjun <wonjun000823@gmail.com>
2023-03-15 16:43:55 +0900

    test: 연구실pc 세팅

--- backend/main.py
+++ backend/main.py
@@ -4,7 +4,7 @@ app = FastAPI()
 
 @app.get("/")
 async def read_root():
-    return {"연구실 컴퓨터로": "환경설정해보기"}
+    return {"연구실 컴퓨터로": "환경설정하기"}
 
 @app.get("/items/{item_id}")
 async def read_item(item_id: int, q: str = None):
