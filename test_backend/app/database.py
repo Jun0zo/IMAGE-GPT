5c94c88b22e438777afb6b5a2a371fb5535ceccf
Jun0zo <joon0zo1022@gmail.com>
2023-03-25 20:47:35 +0900

    configuration: docker실행환경 테스트 #3

---
+++ test_backend/app/database.py
@@ -0,0 +1,18 @@
+from sqlalchemy import *
+from sqlalchemy.orm import sessionmaker
+
+DB_URL = 'mysql+pymysql://{USERNAME}:{PASSWORD}@{HOST}:{PORT}/{DBNAME}'
+
+class engineconn:
+
+    def __init__(self):
+        self.engine = create_engine(DB_URL, pool_recycle = 500)
+
+    def sessionmaker(self):
+        Session = sessionmaker(bind=self.engine)
+        session = Session()
+        return session
+
+    def connection(self):
+        conn = self.engine.connect()
+        return conn
--- test_backend/app/main.py
+++ test_backend/app/main.py
@@ -1,11 +1,15 @@
+import os
 from fastapi import FastAPI
 
 app = FastAPI()
 
+print(os.environ['MYSQL_HOST'])
+print(os.environ['MYSQL_USER'])
+print(os.environ['MYSQL_PASSWORD'])
+
 @app.get("/")
 def read_root():
     return {"Hello": "test"}
-
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str | None = None):
