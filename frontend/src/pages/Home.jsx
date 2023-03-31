5f6c0193ea13204ea85c14118fa78d8f3916f93e
Jun0zo <joon0zo1022@gmail.com>
2023-03-31 11:54:12 +0900

    style: 컴포턴트 정렬만 함 #9

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,31 +1,23 @@
 import NavBar from "../component/NavBar";
 import * as React from "react";
-import Paper from "@mui/material/Paper";
-import InputBase from "@mui/material/InputBase";
-import Divider from "@mui/material/Divider";
-import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
+
+import GPTIcon from "../component/GPTIcon.png";
+
+import {
+  Paper,
+  InputBase,
+  Divider,
+  IconButton,
+  Box,
+  InputLabel,
+  MenuItem,
+  FormControl,
+  Select,
+  TextField,
+} from "@mui/material";
+
 import SearchIcon from "@mui/icons-material/Search";
 import DirectionsIcon from "@mui/icons-material/Directions";
-import Box from "@mui/material/Box";
-import InputLabel from "@mui/material/InputLabel";
-import MenuItem from "@mui/material/MenuItem";
-import FormControl from "@mui/material/FormControl";
-import Select from "@mui/material/Select";
-import TextField from "@mui/material/TextField";
-
-// const useStyles = makeStyles((theme) => ({
-//   formControl: {
-//     margin: theme.spacing(1),
-//     minWidth: 120,
-//     "&:hover": {
-//       border: "none",
-//     },
-//     "&:focus": {
-//       border: "none",
-//     },
-//   },
-// }));
 
 export default function Home() {
   const [age, setAge] = React.useState("");
@@ -37,62 +29,75 @@ export default function Home() {
   return (
     <div>
       <NavBar />
-      <Box sx={{ minWidth: 120, display: "flex" }}>
-        <Paper>
-          <FormControl
-            fullWidth
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        <Box>
+          <img src={GPTIcon} width="300px" alt="main-icon" />
+          <h1 style={{ color: "white", fontSize: "50px" }}>IMAGE-GPT</h1>
+        </Box>
+
+        <Box sx={{ minWidth: 120, display: "flex" }}>
+          <Paper>
+            <FormControl
+              fullWidth
+              sx={{
+                width: "100px",
+                backgroundColor: "white",
+                border: "0px",
+                "&:hover": { border: "none" },
+              }}
+            >
+              <InputLabel id="demo-simple-select-label" shrink={false}>
+                Age
+              </InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={age}
+                label="Age"
+                displayEmpty
+                onChange={handleChange}
+                sx={{ width: "100%" }}
+              >
+                <MenuItem value={10}>Ten</MenuItem>
+                <MenuItem value={20}>Twenty</MenuItem>
+                <MenuItem value={30}>Thirty</MenuItem>
+              </Select>
+            </FormControl>
+          </Paper>
+
+          <Paper
+            component="form"
             sx={{
-              width: "100px",
-              backgroundColor: "white",
-              "&:hover": { border: "none" },
+              p: "2px 4px",
+              display: "flex",
+              alignItems: "center",
+              width: 400,
+              borderLeft: "0px",
             }}
           >
-            <InputLabel id="demo-simple-select-label" shrink={false}>
-              Age
-            </InputLabel>
-            <Select
-              labelId="demo-simple-select-label"
-              id="demo-simple-select"
-              value={age}
-              label="Age"
-              displayEmpty
-              onChange={handleChange}
+            <InputBase
+              sx={{ ml: 1, flex: 1 }}
+              placeholder="검색하실 키워드를 입력하세요"
+              inputProps={{ "aria-label": "search" }}
+            />
+            <IconButton type="button" sx={{ p: "10px" }} aria-label="search">
+              <SearchIcon />
+            </IconButton>
+            <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+            <IconButton
+              color="primary"
+              sx={{ p: "10px" }}
+              aria-label="directions"
             >
-              <MenuItem value={10}>Ten</MenuItem>
-              <MenuItem value={20}>Twenty</MenuItem>
-              <MenuItem value={30}>Thirty</MenuItem>
-            </Select>
-          </FormControl>
-        </Paper>
-
-        <Paper
-          component="form"
-          sx={{
-            p: "2px 4px",
-            display: "flex",
-            alignItems: "center",
-            width: 400,
-          }}
-        >
-          <InputBase
-            sx={{ ml: 1, flex: 1 }}
-            placeholder="Search Image"
-            inputProps={{ "aria-label": "search" }}
-          />
-          <IconButton type="button" sx={{ p: "10px" }} aria-label="search">
-            <SearchIcon />
-          </IconButton>
-          <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
-          <IconButton
-            color="primary"
-            sx={{ p: "10px" }}
-            aria-label="directions"
-          >
-            <DirectionsIcon />
-          </IconButton>
-        </Paper>
-
-        <Box sx={{ minWidth: 120 }}></Box>
+              <DirectionsIcon />
+            </IconButton>
+          </Paper>
+        </Box>
       </Box>
     </div>
   );
