b76d0bfff3708a582e3bd37bc31fe84a99b1e259
Jun0zo <joon0zo1022@gmail.com>
2023-06-04 12:10:00 +0900

    서버 설정

--- frontend/src/config/axiosConfig.js
+++ frontend/src/config/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const server = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL: "http://unilab.kro.kr:8000",
   timeout: 5000,
   headers: {
     "Content-Type": "application/json",
