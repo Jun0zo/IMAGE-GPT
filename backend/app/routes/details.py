06aa1c47cac10bbdf4d304830978d12b82150cde
Jun0zo <joon0zo1022@gmail.com>
2023-06-12 01:20:59 +0900

    get images by video_id 라우터 구현

--- backend/app/routes/details.py
+++ backend/app/routes/details.py
@@ -16,6 +16,7 @@ from models.like import Like
 from models.video import Video
 
 from schemas.responses.ImageDetails import ImageDetails
+from schemas.responses.VideoImages import VideoImages
 
 details = APIRouter()
 
@@ -38,3 +39,13 @@ def get_videos_by_keyword(id: str, db: Session = Depends(get_db)):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Image not found")
 
         
+@details.get("/details/video_images", tags=["details"], response_model=VideoImages)
+def get_images_by_videoid(video_id: str, db: Session = Depends(get_db)):
+    results = db.query(Image).filter(Image.video_id == video_id).all()
+
+    if results:
+        images = [{'id':result.id, 'url':result.url} for result in results]
+
+        return {'result': images}
+    else:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Image not found")
