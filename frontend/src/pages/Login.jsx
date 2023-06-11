9ba7e7c64219ef945a88e533f32c7653bcd4c8ce
Jun0zo <joon0zo1022@gmail.com>
2023-06-11 16:18:51 +0900

    login 뼈대 구축

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,12 +1,36 @@
 import NavBar from "component/NavBar";
+import { GoogleLogin, GoogleOAuthProvider } from '@react-oauth/google';
+
+import { Box } from '@mui/material';
+
+
 // import Button from
 
-function Home() {
+
+
+const Home = () => {
   console.log("!!");
+  const responseMessage = (response) => {
+      console.log(response);
+  };
+  const errorMessage = (error) => {
+      console.log(error);
+  };
+
   return (
     <div>
       <NavBar />
-      <h2>로그인 페이지.</h2>
+      <Box elevation={3} sx={{backgroundColor:"#2a2a2a", width:"500px"}}>
+        <h2>로그인 페이지.</h2>
+
+        <div>
+          <GoogleOAuthProvider clientId={"121079642070-uphpll1qg5mpv0ils68ssk9o0n5q3hdc.apps.googleusercontent.com"}>
+            <GoogleLogin onSuccess={responseMessage} onError={errorMessage} />
+          </GoogleOAuthProvider>
+        </div>
+      </Box>
+      
+      
     </div>
   );
 }
