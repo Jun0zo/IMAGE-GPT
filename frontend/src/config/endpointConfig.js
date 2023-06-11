783c1b9b242561522f4a4b0a439380c36d2f7f28
Jun0zo <joon0zo1022@gmail.com>
2023-06-11 15:17:37 +0900

    이미지 엔트포인트 추가 #5

--- frontend/src/config/endpointConfig.js
+++ frontend/src/config/endpointConfig.js
@@ -1,6 +1,9 @@
 const API_ENDPOINTS = {
   LOGIN: "/login",
   USERS: "/users",
+  SEARCH: {
+    IMAGES: "/images"
+  },
   STATISTICS: {
     SIMILAR_KEYWORDS: "/statistics/similar_keywords",
     RELATED_VIDEOS: "/statistics/related_videos",
