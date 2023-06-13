f7455dfac34697a608b04126d2f183ba09f084cd
Jun0zo <joon0zo1022@gmail.com>
2023-06-13 17:01:52 +0900

    https 적용

--- frontend/src/config/axiosConfig.js
+++ frontend/src/config/axiosConfig.js
@@ -5,6 +5,7 @@ const server = axios.create({
   timeout: 5000,
   headers: {
     "Content-Type": "application/json",
+    "Referrer-Policy": "strict-origin-when-cross-origin",
     // 'Authorization': 'Bearer ' + localStorage.getItem('token')
   },
 });
