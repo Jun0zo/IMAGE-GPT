119637e9c465552e93d7ae371cadfa7a4ae7365f
Jun0zo <joon0zo1022@gmail.com>
2023-06-14 00:25:31 +0900

    통계 데이터 Modal에 삽입 #12

--- frontend/src/pages/Search.jsx
+++ frontend/src/pages/Search.jsx
@@ -1,15 +1,25 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
-import { Box, Button } from "@mui/material";
+import { Box, Button, IconButton, Dialog, AppBar , Divider, ListItemText, ListItem,  List, Toolbar, Slide, Typography   } from "@mui/material";
+import { TransitionProps } from '@mui/material/transitions';
+
+
 
 import NavBar from "component/NavBar";
 import SearchBar from "component/Home/SearchBar";
 import ImageList from "component/Search/ImageList";
 import OverviewChart from "component/Search/OverviewChart";
 
+import QueryStatsIcon from '@mui/icons-material/QueryStats';
+import CloseIcon from '@mui/icons-material/Close';
+
 import server from "config/axiosConfig";
 import API_ENDPOINTS from "config/endpointConfig";
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
 
 const Search = () => {
   const { keyword } = useParams();
@@ -26,6 +36,8 @@ const Search = () => {
 
   const [value, setValue] = useState(keyword);
   const [isRandom, setRandom] = useState(true);
+
+  const [statisticsOpen, setStatisticsOpen] = useState(false)
 
   const loadAll = (is_random) => {
     const fetchStatisticsData = async () => {
@@ -103,9 +115,17 @@ const Search = () => {
     loadAll(isRandom)
   }
 
+  const handleClickOpen = () => {
+    setStatisticsOpen(true);
+  };
+
+  const handleClose = () => {
+    setStatisticsOpen(false);
+  };
+
   useEffect(() => {
     loadAll(isRandom)
-  }, [keyword]);
+  }, [keyword, isRandom]);
   return (
     <div>
       <NavBar />
@@ -115,7 +135,64 @@ const Search = () => {
           {isImagesLoading ? null : <ImageList images={images}/>}  
         </Box>
         <Box sx={{ padding: "30px 0px" }}>
-          <OverviewChart isStatisticsLoading={isStatisticsLoading} 
+          
+        </Box>
+      </div>
+
+      <Button
+        sx={{
+          
+          padding: '10px',
+          color: 'white',
+          borderRadius: '4px',
+        }}
+      >asdf</Button>
+
+      {/* <IconButton aria-label="delete" size="large" color="secondary" sx={{position: 'fixed',
+          bottom: '20px',
+            right: '20px',
+          }} onClick={handleClickOpen}>
+        <QueryStatsIcon fontSize="inherit" />
+      </IconButton> */}
+      <Button variant="contained" startIcon={<QueryStatsIcon />} 
+        sx={{position: 'fixed',
+            bottom: '20px',
+              right: '20px',
+              borderRadius: "20px"
+          }}
+          size="large"
+          onClick={handleClickOpen}>
+        통계 데이터
+      </Button>
+      {keyword}
+
+      <Dialog
+        fullScreen
+        open={statisticsOpen}
+        onClose={handleClose}
+        TransitionComponent={Transition}
+        keyword={keyword}
+        bodyStyle={{ backgroundColor: '#0b1827' }}
+
+      >
+        <Box sx={{backgroundColor: '#0b1827'}}>
+        <AppBar sx={{ position: 'relative', backgroundColor:"rgba(0,0,0,0)" }} elevation={0}>
+          <Toolbar>
+            <IconButton
+              edge="start"
+              color="inherit"
+              onClick={handleClose}
+              aria-label="close"
+            >
+              <CloseIcon />
+            </IconButton>
+            <Typography sx={{ ml: 2, flex: 1 }} variant="h6" component="div">
+              "{keyword}"에 대한 통계 데이터
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Box>
+        <OverviewChart isStatisticsLoading={isStatisticsLoading} 
           statisticsData={statisticsData} 
           keyword={keyword} 
           handleReload={handleReloadAll}
@@ -123,8 +200,9 @@ const Search = () => {
           setRandom={setRandom}
           />
         </Box>
-      </div>
-      {keyword}
+        </Box>
+        
+      </Dialog>
     </div>
   );
 };
